7d33ba910753bb6f644e8926f93cab443ff9d3c4
ajitesh24 <ajiteshprakash24@gmail.com>
2025-02-15 16:14:10 +0530

    adding user details

--- src/main/java/com/example/git/User.java
+++ src/main/java/com/example/git/User.java
@@ -1,4 +1,41 @@
 package com.example.git;
 
 public class User {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private int id;
+    private String name;
+    private String userName;
+    private String password;
+
 }
